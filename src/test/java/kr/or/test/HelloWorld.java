3c16f4c49d56d3fa7a7e0a59644ef8d3c7069f04
wldo9 <wldo9@DESKTOP-BDQQEGJ>
2021-05-26 14:17:52 +0900

    5교시 커밋

--- src/test/java/kr/or/test/HelloWorld.java
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -3,8 +3,9 @@ package kr.or.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// main 자바프로그램의 진입 메서드(자동실행)입니다.
-		System.out.println("헬로 자바!");
+		//자바앱은 진입 메서드가 필요,main()
+		System.out.println("헬로자바!!!");
+
 	}
 
 }
