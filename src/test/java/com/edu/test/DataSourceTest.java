ada4a3e1d426e0ba51ce4fb031b8612e263807e3
wldo9 <wldo9@DESKTOP-BDQQEGJ>
2021-06-07 17:42:13 +0900

    8교시커밋

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -2,7 +2,9 @@ package com.edu.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -33,13 +35,43 @@ public class DataSourceTest {
 	DataSource dataSource;//Inject로 객체를 만들면 메모리 관리를 스프링이 대신해 줌.
 	//Inject 자바8부터 지원, 그럼, 이전 자바7에서 @Autowired 로 객체를 만들었슴
 	
+	//스프링 코딩 시작 순서
+	//M-V-C-사이에 데이터를 입출력하는 임시저장 공간(VO클래스를-멤버 변수 + Get/Set메서드) 생성 
+	//보통 ValueObject클래스는 DB테이블과 1:1
+	//그래서, MemberVO.java VO클래스를 생성.
+	//2. DB(마이바티스)쿼리를 만듭니다.(VO사용됨)
+	@Test
+	public void selectMember() throws Exception{
+		//회원관리 테이블에서 더미로 입력한 100개의 레코드를 출력 메서드 테스트->회원관리목록이 출력
+	}
+	
 	@Test
 	public void oldQueryTest() throws Exception {
 		//스프링빈을 사용하지 않을때 예전 방식: 코딩테스트에서는 스프링설정을 안쓰고, 직접 DB 아이디/암호 입력
 		Connection connection = null;
-		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE","apmsetup");
+		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE2","apmsetup");
 		logger.debug("데이터베이스 직접 접속이 성공 하였습니다. DB종류는 "+ connection.getMetaData().getDatabaseProductName());
-		
+		//직접쿼리를 날립니다. 날리기전 쿼리문장 객체생성statement
+		Statement stmt = connection.createStatement();
+		//위 쿼리문장객체를 만드는 이유? 보안(SQL인젝션공격을 방지)
+		//stmt객체가 없으면, 개발자가 SQL인젝션 방지코딩을 넣어야 합니다.
+		//Insert쿼리문장만듬(아래)
+		//예전 방식으로 더미데이터(샘플데이터)를 100개를 입력합니다.
+		/*
+		 * for(int cnt=0;cnt<100;cnt++) {
+		 * stmt.executeQuery("insert into dept02 values("+cnt+",'디자인부','경기도')"); }
+		 */
+		//인서트,업데이트,삭제시 sql디벨러퍼에서는 커밋이 필수지만, 외부java클래스에서는 자동커밋이 됩니다.
+		//테이블에 입력되어 있는 레코드셋를 select 쿼리 stmt문장으로 가져옴(아래)
+		ResultSet rs = stmt.executeQuery("select * from dept order by deptno");//20년전 작업방식
+		//위에서 저장된 rs객체를 반복문으로 출력(아래)
+		while(rs.next()) {
+			//rs객체의 레코드가 없을때까지 반복
+			logger.debug(rs.getString("deptno")+" "+rs.getString("dname")+
+					" "+rs.getString("loc"));
+		}
+		stmt = null;//메모리 반환
+		rs = null;//메모리 반환
 		connection = null;//메모리 초기화
 	}
 	@Test
