53279ec1ee60ee5075295ce02686b6de2e226c8f
wldo9 <wldo9@DESKTOP-BDQQEGJ>
2021-06-23 17:45:32 +0900

    0623커밋

--- src/main/java/com/edu/controller/AdminController.java
+++ src/main/java/com/edu/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.edu.controller;
 
+import java.io.File;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -13,18 +14,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.edu.service.IF_BoardService;
 import com.edu.service.IF_BoardTypeService;
 import com.edu.service.IF_MemberService;
+import com.edu.util.CommonUtil;
+import com.edu.vo.AttachVO;
 import com.edu.vo.BoardTypeVO;
+import com.edu.vo.BoardVO;
 import com.edu.vo.MemberVO;
 import com.edu.vo.PageVO;
 
 /**
- * 이 클래스는 Admin관리자단을 접근하는 컨트롤러 클래스<-디스패처 서블렛(게이트웨이) 기능을 합니다.
- * 디스패처 서블렛 클래스는 톰캣이 실행될때(web.xml) 제일 먼저 실행되는 클래스, 그래서, 게이트웨이라고 합니다
- * 디스패처 서블릿 실행될때, 컨트롤러의 Request매핑경로를 재 등록합니다.
+ * 이 클래스는 Admin관리자단을 접근하는 컨트롤러 클래스 <- 디스패처 서블렛(게이트웨이) 기능을 합니다.
+ * 디스페처 서블렛 클래스는 톰캣이 실행(web.xml)될때 제일 먼저 실행되는 클래스, 그래서, 게이트웨이라고 합니다.
+ * 디스페처 서블릿 실행될때, 컨트롤러의 Request매핑경로를 재 등록합니다.
  * 변수 Object를 만들어서 jsp로 전송 <-> jsp 폼값을 받아서 Object로 처리
  * @author 김지애
  *
@@ -40,67 +45,189 @@ public class AdminController {
 	@Inject
 	private IF_BoardTypeService boardTypeService;
 	@Inject
-	private IF_BoardService boardService;
-	//게시물 목록은 폼으로 접근하지 않고 URL로 접근하기 때문에 GET방식으로 처리
-	@RequestMapping(value="/admin/board/board_list",method=RequestMethod.GET)
-	public String board_list(@ModelAttribute("pageVO")PageVO pageVO,Model model) throws Exception{//목록폼1
+	private IF_BoardService boardService;//DI으로 스프링빈을 주입해서 객체로 생성
+	@Inject
+	private CommonUtil commonUtil;
+	
+	//게시물 수정처리는 POST로만 접근가능
+	@RequestMapping(value="/admin/board/board_update",method=RequestMethod.POST)
+	public String board_update(@RequestParam("file")MultipartFile[] files,BoardVO boardVO,PageVO pageVO) throws Exception {
+		
+		//기존 등록된 첨부파일 목록 구하기
+		List<AttachVO> delFiles = boardService.readAttach(boardVO.getBno());
+		//1차원 배열의 크기는.length
+		String[] save_file_names = new String[files.length];
+		String[] real_file_names = new String[files.length];
+		int idx= 0;
+		for(MultipartFile file:files) {//files[0],files[1]
+			if(file.getOriginalFilename() != "") {//전송된 첨부파일이 있다면 실행
+				int sun = 0;//DB테이블에 저장된 순서
+				//아래 for 목적:jsp폼에서 기존에 1번 위치에 기존 파일이 있으면, 기존 파일을 지우고 신규파일을 덮어쓰는 로직
+				for(AttachVO file_name:delFiles) {//기존파일을 가져와서 반복하면서 지우기로직
+					if(idx == sun) {
+						File target = new File(commonUtil.getUploadPath(),file_name.getSave_file_name());
+						if(target.exists()) {
+							target.delete();//물리적인 파일 지우는 명령
+						}//if(target.exists()) 
+					}//if(idx == sun)
+					sun = sun +1;
+				}//for (AttachVO file_name:delFiles)
+				//신규파일 업로드 
+				save_file_names[idx] = commonUtil.fileUpload(file);//jsp폼에서 전송파일
+				real_file_names[idx] =file.getOriginalFilename();//UI용 이름임시저장
+			}//if(file.getOriginalFilename() != "")
+		}//for (MultipartFile file.files)
+		String rawContent = boardVO.getContent();
+		String secContent = commonUtil.unScript(rawContent);
+		boardVO.setContent(secContent);
+		String rawTitle = boardVO.getTitle();
+		String secTitle = commonUtil.unScript(rawTitle);
+		boardVO.setTitle(secTitle);
+		boardService.updateBoard(boardVO);//게시물 수정
+		//첨부파일 작업전, 시큐어코딩: 입력/수정시 시큐어코딩적용, 뷰화면에서 시큐어X 
+		
+		String queryString = "bno="+boardVO.getBno()+"&page="+pageVO.getPage()+"&serch_type="+pageVO.getSearch_type();
+		return "redirect:/admin/board/board_view?"+queryString;//수정한 이후에는 board_view페이지로 이동:새로고침 방지하기 위해서 redirect 사용
+	}
+	//게시물 수정폼은 URL쿼리 스트링으로 접근
+	@RequestMapping (value = "/admin/board/board_update_form",method=RequestMethod.GET)
+	public String board_update_form(Model model, @RequestParam("bno")Integer bno, @ModelAttribute("pageVO")PageVO pageVO) throws Exception {
+		BoardVO boardVO = new BoardVO();
+		boardVO = boardService.readBoard(bno);
+		//여기서 첨부파일 배열을 추가(아래)
+		
+		List<AttachVO> listAttachVO = boardService.readAttach(bno);
+		String[] save_file_names=new String[listAttachVO.size()];
+		String[] real_file_names=new String[listAttachVO.size()];
+		int idx=0;
+		//향상된 for문 사용
+		for(AttachVO file_name:listAttachVO) {
+			save_file_names[idx] =file_name.getSave_file_name();
+			real_file_names[idx] =file_name.getReal_file_name();
+			idx = idx + 1;//idx++
+		}
+		boardVO.setSave_file_names(save_file_names);
+		boardVO.setReal_file_names(real_file_names);
+		model.addAttribute("boardVO",boardService.readBoard(bno));
+		return "admin/board/board_update";//.jsp생략
+	}
+	//게시물 삭제는 URL쿼리스트링으로 접근하지 않고, post방식으로 처리.
+	@RequestMapping(value="/admin/board/board_delete", method=RequestMethod.POST)
+	public String board_delete(@RequestParam("bno")Integer bno,PageVO pageVO) throws Exception {
+		//디버그 삭제할 전역변수 경로 확인
+		logger.info("디버그 전역업로드경로: " + commonUtil.getUploadPath());
+		//DB테이블삭제한 이후, 첨부파일부터 있으면 삭제처리. 자바에서 파일핸들링처리
+		//기존 등록된 첨부파일 폴더에서 삭제할 UUID(고유한식별값생성클래스)이름을 추출합니다.(아래)
+		List<AttachVO> delFiles = boardService.readAttach(bno);//해당게시물의 모든 첨부파일 delFiles 에 임시로 담아 놓습니다.
+		boardService.deleteBoard(bno);//첨부파일테이블삭제 후 게시물 테이블 삭제
+		//물리적으로 파일삭제 처리 시작, 향상된 for문사용
+		for(AttachVO file_name:delFiles) {
+			//File클래스는 ("파일의 업로드된 위치","삭제할 파일명");
+			File target = new File(commonUtil.getUploadPath(),file_name.getSave_file_name());
+			if(target.exists()) {
+				target.delete();//물리적인 파일 지우는 명령
+			}
+		}
+		
+		String queryString = "page="+pageVO.getPage()+"&search_type="+pageVO.getSearch_type();
+		return "redirect:/admin/board/board_list?"+queryString;
+	}
+	//게시물 상세보기 폼으로 접근하지 않고 URL쿼리 스트링으로 접근(GET)
+	@RequestMapping(value="/admin/board/board_view", method=RequestMethod.GET)
+	public String board_view(@RequestParam("bno")Integer bno,@ModelAttribute("pageVO")PageVO pageVO, Model model) throws Exception {
+		BoardVO boardVO = boardService.readBoard(bno);
+		
+		//첨부파일 부분 attach데이터도 board_view.jsp로 이동해야 함(아래)
+		List<AttachVO> files = boardService.readAttach(bno);
+		logger.info("debug19: "+ files);
+		//배열객체 생성구조: String[] 배열명 = new String[배열크기];
+		//개발자가 만든 클래스형 객체 boardVO는 개발자가 만든 메서드 사용
+		//반면, List<AttachVO> files List클래스형 객체 files는 내장형 메서드 = .size()
+		String[] save_file_names = new String[files.size()];
+		String[] real_file_names = new String[files.size()];
+		//attach테이블안의 해당bno게시물의 첨부파일 이름 파싱해서 jsp로 보내주는 과정(아래)
+		int cnt = 0;
+		for(AttachVO file_name:files) {//files다수레코드 에서 1개의 레코드씩 추출
+			save_file_names[cnt] = file_name.getSave_file_name();
+			real_file_names[cnt] = file_name.getReal_file_name();
+			cnt = cnt + 1;//cnt++;
+		}
+		//위 for은 세로데이터(다수레코드)를 가로데이터(1레코드이면, 배열)에 담아서 1개 레코드boardVO로 만드게 목적.
+		boardVO.setSave_file_names(save_file_names);//파싱한 결과 Set//다운로드로직
+		boardVO.setReal_file_names(real_file_names);//boardVO에 Set//화면에보이는데
+		model.addAttribute("boardVO", boardVO);//게시물 + 첨부파일 명2개이상
+		//업로드한 파일이 이미지인지 아닌지 확인하는 용도의 데이터 입니다.아래(목적:이미지일때 미리보기 img태그를 사용 하기위해서)
+		model.addAttribute("checkImgArray", commonUtil.getCheckImgArray());
+		return "admin/board/board_view";//.jsp생략
+	}
+	
+	//게시물 목록은 폼으로 접근하지 않고 URL로 접근하기 때문에 GET방식으로처리
+	@RequestMapping(value="/admin/board/board_list", method=RequestMethod.GET)
+	public String board_list(@ModelAttribute("pageVO")PageVO pageVO, Model model) throws Exception {
+		//게시판타입이 null일때 기본값으로 notice를 추가
+		if(pageVO.getBoard_type() == null) {
+			pageVO.setBoard_type("notice");
+		}
 		//페이징처리를 위한 기본값 추가
 		if(pageVO.getPage() == null) {
 			pageVO.setPage(1);
 		}
-		pageVO.setPerPageNum(5);//1페이징 UI하단에서 보여줄 페이징 번호 크기
+		pageVO.setPerPageNum(5);//UI하단에서 보여줄 페이징 번호 크기
 		pageVO.setQueryPerPageNum(5);//토탈 카운트를 구하기전 필수로 필요
 		pageVO.setTotalCount(boardService.countBoard(pageVO));
 		
-		model.addAttribute("listBoardVO",boardService.selectBoard(pageVO));
-		return "admin/board/board_list";//jsp생략
+		model.addAttribute("listBoardVO", boardService.selectBoard(pageVO));
+		return "admin/board/board_list";//.jsp생략
 	}
-	//jsp에서 게시판생성관리에  Get/Post 접근할때 URL을 bbs_type로 지정합니다.
-	//왜 board_type하지않고, bbs_type하는 이유는 왼쪽메뉴 고정시키는 로직에서 경로 board와 겹치지 않게 하기 위해
-	@RequestMapping(value="/admin/bbs_type/bbs_type_list",method=RequestMethod.GET)
-	public String selectBoardTypeList(Model model) throws Exception{//목록폼1
-		//아래 모델은 AOP기능중 ControllerAdivce 인터페이스로 구현했기 때문에 아래는 실행안함.
-		//model.addAttribute("listBoardTypeVO",boardTypeService.selectBoardType());boardTypeService.selectBoardType();
-		return "admin/bbs_type/bbs_type_list";//상대경로 일때는  views폴더가 root(최상위)
+	//jsp에서 게시판생성관리에 Get/Post 접근할때 URL을 bbs_type로 지정합니다.
+	//왜 board_type하지않고, bbs_type하는 이유는 왼쪽메뉴 고정시키는 로직에서 경로 board와 겹치지 않도록
+	@RequestMapping(value="/admin/bbs_type/bbs_type_list", method=RequestMethod.GET)
+	public String selectBoardTypeList(Model model) throws Exception {//목록폼1
+		//아래 모델은 AOP기능중 ControllerAdvice 인터페이스로 구현했기 때문에 아래는 실행안함.
+		//model.addAttribute("listBoardTypeVO", boardTypeService.selectBoardType());
+		return "admin/bbs_type/bbs_type_list";//상대경로일때는 views폴더가 root(최상위)
 	}
-	//bbs_type_list,jsp에서 게시판생성 버튼을 클릭했을때 이동하는 폼 경로
-	@RequestMapping(value="/admin/bbs_type/bbs_type_insert",method=RequestMethod.GET)
-	public String insertBoardTypeForm() throws Exception{//입력폼1
+	//bbs_type_list.jsp에서 게시판생성 버튼을 클릭했을때 이동하는 폼 경로 
+	@RequestMapping(value="/admin/bbs_type/bbs_type_insert", method=RequestMethod.GET)
+	public String insertBoardTypeForm() throws Exception {//입력폼1
 		return "admin/bbs_type/bbs_type_insert";//.jsp생략
 	}
-	//bbs_type_insert.jsp의 입력폼에서 전송된 값을 BoardTypeVO 자동담겨서 담아서 {구현}단, 자동으로 값이 바인딩되려면,폼name과,VO 멤버변수명 동일 해야함. 
-	@RequestMapping(value="/admin/bbs_type/bbs_type_insert",method=RequestMethod.POST)
-	public String insertBoardType(BoardTypeVO boardTypeVO) throws Exception{//입력처리1
+	//bbs_type_insert.jsp의 입력폼에서 전송된 값을 boardTypeVO 자동담겨서 {구현} 단, 자동으로 값이 바인딩되려면, 폼name과, VO 멤버변수명 동일해야함. 
+	@RequestMapping(value="/admin/bbs_type/bbs_type_insert", method=RequestMethod.POST)
+	public String insertBoardType(BoardTypeVO boardTypeVO) throws Exception {//입력처리1
 		boardTypeService.insertBoardType(boardTypeVO);
-		return "redirect:/admin/bbs_type/bbs_type_list";//리다이렉트(뒤로가기 데이터사라짐)는 절대경로, forward:이동이 가능 (뒤로가기 데이터가 살아있음)
+		return "redirect:/admin/bbs_type/bbs_type_list";
+		//리다이렉트(뒤로가기 데이터사라짐)는 절대경로, forward:이동이 가능(뒤로가기 데이터가 살아있음)
+		//쇼핑몰에서 결제화면을 처리 후 뒤로가기를 누르면, 리다이렉트는 데이터가 사라지기때문에 재결제 불가
+		//forward로 결제화면을 처리 후 뒤로가기를 누르면, 재결제가 발생됩니다. 이러면 않되기때문에 사용안함. 
 	}
-	
-	//게시판 생성관리는 이 기능은 사용자단에서 UI를 사용할 일이 없기때문,Read,Update를 1개로 사용
-	@RequestMapping(value="admin/bbs_type/bbs_type_update",method=RequestMethod.GET)
-	public String updateBoardTypeForm(@RequestParam("board_type")String board_type,Model model) throws Exception{//수정폼
-		model.addAttribute("boardTypeVO",boardTypeService.readBoardType(board_type));//서식model.addAttbute("jsp변수","데이터객체");
-		return "admin/bbs_type/bbs_type_update";//jsp생략
+	//게시판 생성관리는 이 기능은 사용자단에서 UI를 사용할 일이 없기때문에, Read, Update를 1개로 사용.
+	@RequestMapping(value="/admin/bbs_type/bbs_type_update", method=RequestMethod.GET)
+	public String updateBoardTypeForm(@RequestParam("board_type")String board_type, Model model) throws Exception {//수정폼1
+		model.addAttribute("boardTypeVO", boardTypeService.readBoardType(board_type));
+		//서식model.add~("jsp변수로담아서 view화면으로 보냄","서비스에서 쿼리실행한 데이터객체");
+		return "admin/bbs_type/bbs_type_update";//.jsp생략
 	}
-	@RequestMapping(value="admin/bbs_type/bbs_type_update",method=RequestMethod.POST)
-	public String updateBoardType(BoardTypeVO boardTypeVO) throws Exception{//수정처리1
+	@RequestMapping(value="/admin/bbs_type/bbs_type_update", method=RequestMethod.POST)
+	public String updateBoardType(BoardTypeVO boardTypeVO) throws Exception {//수정처리1
 		boardTypeService.updateBoardType(boardTypeVO);
-		return "redirect:/admin/bbs_type/bbs_type_update?board_type="+boardTypeVO.getBoard_type();//.jsp생략
+		return "redirect:/admin/bbs_type/bbs_type_update?board_type="+boardTypeVO.getBoard_type();//수정한 이후 수정폼을 GET방식으로 이동
 	}
-	@RequestMapping(value="admin/bbs_type/bbs_type_delete",method=RequestMethod.POST)
-	public String deleteBoardType(@RequestParam("board_type")String board_type) throws Exception{//삭제처리1
+	@RequestMapping(value="/admin/bbs_type/bbs_type_delete", method=RequestMethod.POST)
+	public String deleteBoardType(@RequestParam("board_type")String board_type) throws Exception {//삭제처리1
 		boardTypeService.deleteBoardType(board_type);//삭제서비스 호출(실행) 끝
 		return "redirect:/admin/bbs_type/bbs_type_list";//.jsp생략
 	}
-	//===============================================================
+	//=============================================================
 	//아래 경로는 회원신규등록 폼을 호출하는 URL쿼리스트링으로 보낸것을 받을때는 GET방식으로 받습니다.
 	@RequestMapping(value="/admin/member/member_insert_form", method=RequestMethod.GET)
-	public String insertMemberForm(@ModelAttribute("pageVO")PageVO pageVO) throws Exception{
+	public String insertMemberForm(@ModelAttribute("pageVO")PageVO pageVO) throws Exception {
 		
 		return "admin/member/member_insert";//.jsp는 생략
 	}
 	//아래 경로는 회원신규등록을 처리하는 서비스를 호출하는 URL
 	@RequestMapping(value="/admin/member/member_insert", method=RequestMethod.POST)
-	public String insertMember(PageVO pageVO,MemberVO memberVO) throws Exception{
+	public String insertMember(PageVO pageVO,MemberVO memberVO) throws Exception {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String rawPassword = memberVO.getUser_pw();//원시 패스워드값
 		String encPassword = passwordEncoder.encode(rawPassword);
@@ -108,21 +235,20 @@ public class AdminController {
 		memberService.insertMember(memberVO);
 		return "redirect:/admin/member/member_list";//.jsp생략
 	}
-	
 	//아래 경로는 수정처리를 호출=DB를 변경처리함.
 	@RequestMapping(value="/admin/member/member_update", method=RequestMethod.POST)
 	public String updateMember(MemberVO memberVO, PageVO pageVO) throws Exception {
 		//update 서비스만 처리하면 끝
 		//업데이트 쿼리서비스 호출하기 전 스프링시큐리티 암호화 적용합니다.
 		String rawPassword = memberVO.getUser_pw();
-		if(!rawPassword.isEmpty()) {//수정폼에서 암호 입력값이 비어있지 않을때만 아래로직실행.
+		if(!rawPassword.isEmpty()) {//수정폼에서 암호 입력값이 비어있지 않을때만 아래 로직실행.
 			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 			String encPassword = passwordEncoder.encode(rawPassword);
 			memberVO.setUser_pw(encPassword);
 		}
 		memberService.updateMember(memberVO);//반환값이 없습니다.
 		//redirect로 페이지를 이동하면, model로 담아서 보낼수 없습니다. 쿼리스트링(URL?)으로 보냅니다.
-		String queryString = "user_id="+memberVO.getUser_id()+"&page="+pageVO.getPage()+"&search_type="+pageVO.getSearch_type()+"&search_keyword="+pageVO.getSearch_keyword();
+		String queryString = "user_id="+memberVO.getUser_id()+"&page="+pageVO.getPage()+"&search_type="+pageVO.getSearch_type();
 		return "redirect:/admin/member/member_update_form?"+queryString;
 	}
 	//아래 경로는 수정폼을 호출=화면에 출력만=렌더링만 
--- src/main/java/com/edu/util/CommonUtil.java
+++ src/main/java/com/edu/util/CommonUtil.java
@@ -1,45 +1,106 @@
 package com.edu.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import javax.annotation.Resource;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.edu.service.IF_MemberService;
 import com.edu.vo.MemberVO;
 
 /**
- * 이 클래스는 이 프로젝트에서 공통으로 사용하는 유틸리티기능을 모아놓은 클래스.
+ * 이 클래스는 이 프로젝트에서 공통으로 사용하는 유틸리티기능을 모아놓은 클래스. 
  * @author 김지애
- * 컨트롤러 기능 @Controller(jsp와 바인딩명시)
- * 콤포턴트 @Component는 MVC가 아닌 기능들을 모아놓은 스프링빈 명시,여기서는 jsp와 바인딩이 필요해서 사용안함
+ * 컨트롤러 기능 @Controller(jsp와 바인딩이 필요할때는 필수 애노테이션 입니다.)
+ * 콤포턴트 @Component는 MVC가 아닌 기능들을 모아놓은 스프링빈 명시, 여기서는 jsp와 바인딩이 필요해서 사용않함 
  */
 @Controller
 public class CommonUtil {
 	//멤버변수생성(아래)
 	private Logger logger = LoggerFactory.getLogger(CommonUtil.class);
 	@Inject
-	private IF_MemberService memberService;//스프링빈을 주입 받아서(DI) 객체준비
-	//RestAPI서버 맛보기ID중복체크 (제대로 만들면 @RestController 사용)
+	private IF_MemberService memberService;//스프링빈을 주입받아서(DI) 객체준비
+	
+	//XSS 크로스사이트스크립트 방지용 코드로 파싱하는 메서드 (아래)
+	public String unScript(String data) {
+		//if(data ==null || data.trim().equals("")) {
+			if(data.isEmpty()) {
+				return "";
+			}
+				String ret = data;
+				ret = ret.replaceAll("<(S|s)(C|c)(R|r)(I|i)(P|p)(T|t)", "&lt;script");
+		        ret = ret.replaceAll("</(S|s)(C|c)(R|r)(I|i)(P|p)(T|t)", "&lt;/script");
+		        ret = ret.replaceAll("<(O|o)(B|b)(J|j)(E|e)(C|c)(T|t)", "&lt;object");
+		        ret = ret.replaceAll("</(O|o)(B|b)(J|j)(E|e)(C|c)(T|t)", "&lt;/object");
+		        ret = ret.replaceAll("<(A|a)(P|p)(P|p)(L|l)(E|e)(T|t)", "&lt;applet");
+		        ret = ret.replaceAll("</(A|a)(P|p)(P|p)(L|l)(E|e)(T|t)", "&lt;/applet");
+		        ret = ret.replaceAll("<(E|e)(M|m)(B|b)(E|e)(D|d)", "&lt;embed");
+		        ret = ret.replaceAll("</(E|e)(M|m)(B|b)(E|e)(D|d)", "&lt;embed");
+		        ret = ret.replaceAll("<(F|f)(O|o)(R|r)(M|m)", "&lt;form");
+		        ret = ret.replaceAll("</(F|f)(O|o)(R|r)(M|m)", "&lt;form");
+		        
+		        return ret;
+		
+	}
+	//첨부파일 업로드/다운로드/삭제/인서트/수정에 모두 사용될 저장경로를 1개지정해서 [전역]으로사용
+	@Resource(name="uploadPath")
+	private String uploadPath;//root-context 업로드경로 클래스빈의 id값을 받아서 String변수 입력
+	public String getUploadPath() {
+		return uploadPath;
+	}
+
+	//첨부파일이 이미지인지 아닌지 확인하는 데이터생성
+	private ArrayList<String> checkImgArray = new ArrayList<String>() {
+		{
+		add("gif");
+		add("jpg");
+		add("jpeg");
+		add("png");
+		add("bmp");
+		}
+	};
+	public ArrayList<String> getCheckImgArray() {
+		return checkImgArray;
+	}
+	
+	//RestAPI서버 맛보기ID중복체크(제대로 만들면 @RestController 사용)
 	@RequestMapping(value="/id_check", method=RequestMethod.GET)
 	@ResponseBody //반환받은 값의 헤더값을 제외하고, 내용(body)만 반환하겠다는 명시
 	public String id_check(@RequestParam("user_id")String user_id) throws Exception {
 		//중복아이디를 체크로지(아래)
-		String memberCnt = "1";//중복ID가 없을때, 기본값 0
-		if(!user_id.isEmpty()) {//user_id가 공백이 아니라면,
+		String memberCnt = "1";//중복ID가 있을때, 기본값 1
+		if(!user_id.isEmpty()) {//!주의 user_id가 공백이 아니라면,
 			MemberVO memberVO = memberService.readMember(user_id);
-			logger.info("디버그:"+memberVO);//user_id를 공백을 전송해도 null 이기때문에 조건 추가 필요
-			if(memberVO == null) {//!주의 중복아이디가 존재하지 않으면 {}안을 실행
-				//isEmpty는 null체크 + "" 공백체크 모두 포함
+			logger.info("디버그: " + memberVO);//user_id를 공백을 전송해도 null이기때문에 조건 추가필요
+			if(memberVO == null) {//중복아이디가 존재하지 않으면 {}안을 실행
 				memberCnt = "0";
 			}
 		}
 		return memberCnt;//0.jsp 이렇게 작동하지 않습니다. 이유는 @ResponseBody때문이고, RestAPI는 값만 반환
-		
+	}
+
+	//파일 업로드 공통 메서드(Admin컨트롤러 에서 사용+Home컨트롤러 사용) 
+	public String fileUpload(MultipartFile file) throws IOException {
+		// TODO UUID클래스로 저장될 고유식별(PK) 파일명을 생성 후 물리적으로 저장
+		String realFileName = file.getOriginalFilename();
+		// 폴더에 저장할 PK 파일명을 생성(아래)
+		UUID uid = UUID.randomUUID();//유니크 ID값 생성
+		String saveFileName = uid.toString() + "." + StringUtils.getFilenameExtension(realFileName);
+		byte[] fileData = file.getBytes();
+		File target = new File(uploadPath,saveFileName);
+		return null;
 	}
 }
