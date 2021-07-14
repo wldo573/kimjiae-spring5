7ba78ac7c1b6ad612f33432a157630cc792b7934
wldo9 <wldo9@DESKTOP-BDQQEGJ>
2021-07-14 16:59:45 +0900

    0714커밋

--- src/main/java/com/edu/util/NaverLoginApi.java
+++ src/main/java/com/edu/util/NaverLoginApi.java
@@ -1,24 +1,24 @@
 package com.edu.util;
 
-import com.github.scribejava.core.builder.api.BaseApi;
 import com.github.scribejava.core.builder.api.DefaultApi20;
 
-public class NaverLoginApi extends DefaultApi20{
+public class NaverLoginApi extends DefaultApi20 {
 
 	@Override
 	public String getAccessTokenEndpoint() {
-		// 
-		return null;
+		// 토큰체크용 Endpoint는 네이버의 Rest-Api URL을 명시합니다.
+		//(예, 중복ID체크 RestApi URL /id_check?~)
+		return "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code";
 	}
 
 	@Override
 	protected String getAuthorizationBaseUrl() {
-		// 
-		return null;
+		// 인증체크용 RestApi URL
+		return "https://nid.naver.com/oauth2.0/authorize";
 	}
-	
-	//싱클톤 인스턴스 객체를 생성하기 위해서 인스턴스 홀더 클래스 상수(static,final)변수를 생성합니다.
-	private static class InstanceHolder{
+
+	//싱클톤 인스트턴스 객체를 생성하기 위해서 인스턴스 홀더 클래스 상수(static,final)변수를 생성합니다.
+	private static class InstanceHolder {
 		private static final NaverLoginApi INSTANCE = new NaverLoginApi();
 	}
 	public static NaverLoginApi instance() {
@@ -26,5 +26,4 @@ public class NaverLoginApi extends DefaultApi20{
 		return InstanceHolder.INSTANCE;
 	}
 
-
-	}
+}
